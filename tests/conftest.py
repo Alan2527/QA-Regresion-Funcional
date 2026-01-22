f2fad3c2faebf9a8b9b9abdc8a53f29710d38e0d
Alan2527 <98465440+Alan2527@users.noreply.github.com>
2026-01-22 09:58:07 -0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,59 +1,42 @@
 import pytest
-import allure
-import time
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-def test_configuracion_notificaciones(driver):
-    url = "https://tn.com.ar/"
-    try:
-        driver.get(url)
-        wait = WebDriverWait(driver, 25)
-        
-        # 1. Abrir campana
-        boton_campana = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "font__action")))
-        driver.execute_script("arguments[0].click();", boton_campana)
-        
-        # 2. Click en "Activá las notificaciones"
-        selector_activar = "//button[contains(., 'Activá')]"
-        boton_activar = wait.until(EC.element_to_be_clickable((By.XPATH, selector_activar)))
-        driver.execute_script("arguments[0].click();", boton_activar)
-
-        # 3. Activar los 10 switches
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, "toggle-switch")))
-        for i in range(1, 11):
-            try:
-                xpath_span = f'//*[@id="fusion-app"]/header/div/div[1]/div[2]/div[1]/div[2]/div/div/div[2]/div[{i}]/label/span'
-                span_slider = driver.find_element(By.XPATH, xpath_span)
-                driver.execute_script("arguments[0].click();", span_slider)
-                time.sleep(0.3)
-            except: continue
-
-        # --- VALIDACIÓN DE PERSISTENCIA ---
-        # 4. CERRAR
-        campana_cierre = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "font__action")))
-        driver.execute_script("arguments[0].click();", campana_cierre)
-        time.sleep(3) # Pausa para que el modal cierre bien
-        
-        # 5. REABRIR
-        campana_reabrir = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "font__action")))
-        driver.execute_script("arguments[0].click();", campana_reabrir)
-        
-        # 6. ESPERA PARA RENDERIZADO (Con Try/Except para no romper el test)
-        try:
-            # Intentamos esperar los items, pero si tarda, no matamos el proceso
-            wait_corto = WebDriverWait(driver, 10)
-            wait_corto.until(EC.visibility_of_any_elements_located((By.CLASS_NAME, "notification-setting-item")))
-        except:
-            print("Aviso: Los elementos tardaron en ser detectados, tomando captura de seguridad.")
-            
-        time.sleep(4) # Tiempo extra para que los estilos (azul) se vean bien
-        
-    finally:
-        # CAPTURA FINAL: Se toma pase lo que pase en el bloque anterior
-        allure.attach(
-            driver.get_screenshot_as_png(), 
-            name="Captura_Final_Estilos_Originales", 
-            attachment_type=allure.attachment_type.PNG
-        )
+@pytest.fixture(scope="function")
+def driver():
+    # --- CONFIGURACIÓN DE CHROME ---
+    options = Options()
+    options.add_argument("--headless=new")  # Modo sin ventana para GitHub Actions
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1280,1000") # Tamaño de ventana fijo para capturas consistentes
+    
+    driver = webdriver.Chrome(options=options)
+    
+    # --- BLOQUEO SELECTIVO DE RED (CDP) ---
+    # Bloqueamos solo lo pesado/molesto para que la página sea liviana 
+    # pero conserve los ESTILOS (CSS) y FUENTES para que se vea bien.
+    driver.execute_cdp_cmd("Network.setBlockedURLs", {
+        "urls": [
+            # Bloqueamos imágenes pesadas
+            "*.jpg", "*.jpeg", "*.png", "*.gif", "*.webp", "*.svg", 
+            # Bloqueamos trackers y publicidad (lo que suele trabar el sitio)
+            "*google-analytics.com*", "*doubleclick.net*", "*googletagservices.com*",
+            "*facebook.net*", "*twitter.com*", "*ads*", "*metrics*", "*video*",
+            # Bloqueamos otros elementos innecesarios
+            "*.mp4", "*.m4v"
+        ]
+    })
+    driver.execute_cdp_cmd("Network.enable", {})
+    
+    # --- TIMEOUTS DE SEGURIDAD ---
+    # Tiempo máximo para cargar la estructura de la página
+    driver.set_page_load_timeout(45)
+    # Tiempo máximo para que los scripts internos respondan
+    driver.set_script_timeout(15)
+    
+    yield driver
+    
+    # --- CIERRE ---
+    driver.quit()
