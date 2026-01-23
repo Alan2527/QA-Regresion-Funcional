67c57dfa0918c302c2dbefc599254344b39ef1fd
Alan2527 <98465440+Alan2527@users.noreply.github.com>
2026-01-23 15:04:06 -0300

    Update masleidas.py

--- tests/masleidas.py
+++ tests/masleidas.py
@@ -1,32 +1,28 @@
 import pytest
 import allure
-import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_most_read_component(driver):
-    url = "https://tn.com.ar/politica/2026/01/19/el-gobierno-anuncio-que-tv-publica-transmitira-los-partidos-de-la-seleccion-argentina-durante-el-mundial-2026/"
+def test_ranking_mas_leidas(driver):
+    wait = WebDriverWait(driver, 20)
+    driver.get("https://tn.com.ar/")
+    
     try:
-        driver.get(url)
-        wait = WebDriverWait(driver, 25)
+        # 1. Localizar el ranking
+        seccion_mas_leidas = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "most-read")))
+        print("INFO: Sección 'Más Leídas' detectada en la Home.")
+
+        # 2. Click en la segunda noticia del ranking
+        xpath_noticia_2 = '//*[@id="fusion-app"]/main/div[1]/div/div[2]/div[1]/section/article[2]/div/a/h2'
+        noticia = wait.until(EC.element_to_be_clickable((By.XPATH, xpath_noticia_2)))
+        titulo = noticia.text
         
-        # Tu XPath exacto
-        xpath_principal = '//*[@id="fusion-app"]/div[9]/aside/div[2]'
-        container = wait.until(EC.presence_of_element_located((By.XPATH, xpath_principal)))
-        
-        # Centrar elemento y esperar a que la UI se estabilice
-        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", container)
-        time.sleep(2)
-        
-        # Validar noticias
-        stories = container.find_elements(By.CLASS_NAME, "brick_most-read__story")
-        assert len(stories) > 0, "No se encontraron noticias"
-        
+        noticia.click()
+        print(f"ÉXITO: Se navegó a la nota: '{titulo}' desde Más Leídas")
+
+    except Exception as e:
+        print(f"ERROR: No se pudo interactuar con Maás Leídas {e}")
+        pytest.fail(f"Error en Más Leidas: {e}")
     finally:
-        # La captura siempre se adjunta, incluso si falla el assert
-        allure.attach(
-            driver.get_screenshot_as_png(), 
-            name="Captura_MasLeidas", 
-            attachment_type=allure.attachment_type.PNG
-        )
+        allure.attach(driver.get_screenshot_as_png(), name="MasLeidas_Final", attachment_type=allure.attachment_type.PNG)
